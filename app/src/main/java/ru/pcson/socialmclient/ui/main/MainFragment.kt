9eb019872197a5c065cc94aa651e9bbb9b60cf90
admin <admin@pcson.ru>
2018-07-04 16:50:37 +1000

    Initial commit

---
+++ app/src/main/java/ru/pcson/socialmclient/ui/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package ru.pcson.socialemployer.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.pcson.socialemployer.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.content_main_social, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
